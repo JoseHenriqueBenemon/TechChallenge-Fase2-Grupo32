8611e5408549fe77e40615e9dd429cc6e4088cff
Milena Basso <174759134+mlnbss@users.noreply.github.com>
2024-08-09 05:48:34 -0300

    feat: add async function putPost to controller

--- src/controllers/post.controller.ts
+++ src/controllers/post.controller.ts
@@ -1,6 +1,7 @@
 import { getPostById } from "../services/post/getPostById.service";
 import { getAllPosts } from "../services/post/getAllPosts.service";
 import { createPost } from "../services/post/createPost.service";
+import { updatePost } from "../services/post/updatePost.service";
 import { getPostsByKeyword } from "../services/post/getPostsByKeyword.service";
 import { NextFunction, Request, Response } from "express";
 import { postBodySchema, postParamsSchema, postQuerySchema, searchPostParamsSchema } from "../validation/post.schema";
@@ -21,7 +22,8 @@ export async function getPost(req: Request, res: Response, next: NextFunction): 
     try {
         const { id } = postParamsSchema.parse(req.params);
         
-        const post = await getPostById(id);
+        const post = await getPostById(id, "GET");
+
         res.status(200).json(formatPostResponse(post));
     } catch(error) {
         next(error);
@@ -50,3 +52,17 @@ export async function addPost(req: Request, res: Response, next: NextFunction): 
         next(error);
     }
 }
+
+export async function putPost(req: Request, res: Response, next: NextFunction): Promise<void> {
+    try {
+        const { id } = postParamsSchema.parse(req.params);
+        const { title, description, category_subject, status, limit_date } = postBodySchema.partial().parse(req.body);
+        const newPost = await updatePost(id, { title, description, category_subject, status, limit_date });
+
+        res.status(200).json(formatPostResponse(newPost));
+        } catch (error) {
+            next(error);
+        }
+    }
+
+    
