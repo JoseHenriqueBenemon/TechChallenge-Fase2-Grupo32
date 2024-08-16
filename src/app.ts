8970a9a121ca9d56defdd6d11bf79dd8d407d3c7
José Henrique Benemon <jh.kito@hotmail.com>
2024-08-15 23:27:24 -0300

    Starting Swagger

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,29 @@ import userRoutes from "./routes/user.routes";
 import express, { Express } from "express";
 import { errorHandler } from "./middlewares/error.handler";
 import { authValidation } from "./middlewares/authentication";
+import swaggerUi from 'swagger-ui-express';
 import 'express-async-errors';
+import { env } from "./configs/env-config";
 
 export const app: Express = express();
+const port = 3000;
+
+// Documento Swagger (OpenAPI) manualmente definido
+const swaggerDocument = {
+  openapi: '3.0.0',
+  info: {
+    title: 'TechChallenge',
+    version: '1.0.0',
+    description: 'Documentação grupo 32',
+  },
+  servers: [
+    {
+      url: `http://${env.HOST}:${env.PORT}`,
+    },
+  ],
+}
+
+app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.use(express.json());
 app.use(authValidation);
