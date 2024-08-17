74ca200c3532608782e089853a5512cbc8b13ed4
José Henrique Benemon <jh.kito@hotmail.com>
2024-08-17 19:14:01 -0300

    Certs CA Insert

--- src/configs/database.ts
+++ src/configs/database.ts
@@ -2,6 +2,8 @@ import { DataSource } from "typeorm";
 import { env } from "./env-config";
 import { Post } from "../models/post.model";
 import { User } from "../models/user.model";
+import fs from 'fs';
+import path from 'path';
 
 export const appDataSource = new DataSource({
     type: "postgres",
@@ -11,6 +13,10 @@ export const appDataSource = new DataSource({
     password: env.DATABASE_PASSWORD,
     database: env.DATABASE_NAME,
     logging: env.NODE_ENV === "development",
+    ssl: {
+        rejectUnauthorized: true,
+        ca: fs.readFileSync(path.join(__dirname, '../../certs/ca.pem')).toString()
+    },
     entities: [Post, User],
     migrations: [],
     synchronize: true
